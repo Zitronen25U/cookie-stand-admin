88d71099a99ad3c12bc418df77074db8955687ca
Brian Lemons <brian_lemons@hotmail.com>
2021-07-07 01:14:05 -0700

    adding the final ish lab

---
+++ pages/formOutput.js
@@ -0,0 +1,10 @@
+function FormOutput(){
+    return(
+        <div className="flex justify-center">
+            <p className="flex">Report Table Coming Soon...</p>
+            <p className="flex">Location: Barcelona", "min customers: 2", "max customers: 4", "avg cookies: 2.5</p>
+        </div>
+    )
+}
+
+export default FormOutput
---
+++ pages/header.js
@@ -0,0 +1,8 @@
+
+function Header(){
+    return (
+      <h1 className="bg-green-200 text-2xl px-4 py-2"> Cookie Stand Admin</h1>
+    )
+  }
+
+export default Header 
